76c14a671c8648e8fc744a82703988e1bea6287d
Kazato Sugimoto <uiureo@gmail.com>
2013-05-24 15:57:36 +0900

    update example

--- example/basic.rb
+++ example/basic.rb
@@ -5,12 +5,11 @@ include SimpleAsm
 s = Simple.new do
   add r0, r1
   sub 0, 1
-  ld 1, 0, 111
-  st 0, 1, 101
+  ld 1, 0, 4
+  st 0, 1, 8
   li 1, 10
   b 2, 11
   be 255
 end
 
-puts s.to_s
 puts s.to_mif
